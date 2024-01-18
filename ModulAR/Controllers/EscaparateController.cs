1e6717e6a6fd1f7007b0852503bbec63cde7d90f
Macarena Caro <maccarcor@alu.edu.gva.es>
2024-01-19 00:19:50 +0100

    Escaparate, check, con fotos y filtro

---
+++ ModulAR/Controllers/EscaparateController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModulAR.Data;
+
+namespace ModulAR.Controllers
+{
+    public class EscaparateController : Controller
+    {
+        private readonly MvcTiendaContexto _context;
+
+        public EscaparateController(MvcTiendaContexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? categoryId, string strCadenaBusqueda)
+        {
+            ViewData["BusquedaActual"] = strCadenaBusqueda;
+
+            // Cargar datos de las categorías
+            var categorias = await _context.Categorias.ToListAsync();
+            ViewData["Categorias"] = categorias;
+
+            // Cargar datos de los productos
+            var productosQuery = _context.Productos.AsQueryable();
+
+            if (categoryId.HasValue)
+            {
+                productosQuery = productosQuery.Where(p => p.CategoriaId == categoryId);
+            }
+
+            var productos = await productosQuery.ToListAsync();
+
+            ViewData["Productos"] = productos; // Agregar esta línea
+
+            return View(productos);
+        }
+    }
+}
---
+++ ModulAR/Models/Escaparate.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ModulAR.Models
+{
+    public class Escaparate
+    {
+
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+        public int ProductoId { get; set; }
+        public virtual Producto? Producto { get; set; }
+    }
+}
