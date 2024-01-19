b745be0c7b1bce6b78dc4a81298505f7e0b4142e
Macarena Caro <maccarcor@alu.edu.gva.es>
2024-01-19 11:03:29 +0100

    Sistemas de autenticacion

--- ModulAR/Program.cs
+++ ModulAR/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ModulAR;
 using ModulAR.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,9 +17,26 @@ options.UseSqlServer(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>() //linea nueva
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+//PARRAFO NUEVO:
+// Configuración de los servicios de ASP.NET Core Identity
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings. Configuración de las características de las contraseñas
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    //options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireNonAlphanumeric = false;
+    //options.Password.RequireUppercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
 
 var app = builder.Build();
 
@@ -46,5 +64,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+
+// Crear los roles y el administrador predeterminados
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.InitializeAsync(services).Wait();
+}
 
 app.Run();
---
+++ ModulAR/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ModulAR
+{
+    public class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            // Comprobar y crear los roles predeterminados
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            await CrearRolesAsync(roleManager);
+            // Comprobar y crear el administrador predeterminado
+            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+            await CrearAdminAsync(userManager);
+        }
+        private static async Task CrearRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            // Si no existe, se crea el rol predeterminado "Administrador"
+            string nombreRol = "Administrador";
+            var yaExiste = await roleManager.RoleExistsAsync(nombreRol);
+            if (!yaExiste)
+                await roleManager.CreateAsync(new IdentityRole(nombreRol));
+            // Si no existe, se crea el rol predeterminado "Usuario"
+            nombreRol = "Usuario";
+            yaExiste = await roleManager.RoleExistsAsync(nombreRol);
+            if (!yaExiste)
+                await roleManager.CreateAsync(new IdentityRole(nombreRol));
+        }
+        private static async Task CrearAdminAsync(UserManager<IdentityUser> userManager)
+        {
+            // Comprobar si existe el administrador predetermindado
+            var testAdmin = userManager.Users
+            .Where(x => x.UserName == "admin@modular.com")
+            .SingleOrDefault();
+            if (testAdmin != null) return;
+            testAdmin = new IdentityUser
+            {
+                UserName = "admin@modular.com",
+                Email = "admin@modular.com"
+            };
+            string admPasswd = "modular123";
+            // Si no existe, se crea el administrador predeterminado "admin@modular.com"
+            IdentityResult userResult;
+            userResult = await userManager.CreateAsync(testAdmin, admPasswd);
+            // Se agrega el rol "Administrador" al administrador predeterminado
+            if (userResult.Succeeded)
+            {
+                await userManager.AddToRoleAsync(testAdmin, "Administrador");
+            }
+        }
+    }
+
+}
